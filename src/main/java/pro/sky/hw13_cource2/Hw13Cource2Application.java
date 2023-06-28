33db2a5ffc9ac53cc46d7f1433234eb5930512a4
MariaLanss <maruska0990@gmail.com>
2023-06-28 16:53:09 +0300

    .

---
+++ src/main/java/pro/sky/hw13_cource2/Hw13Cource2Application.java
@@ -0,0 +1,13 @@
+package pro.sky.hw13_cource2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw13Cource2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw13Cource2Application.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/hw13_cource2/Hw13Cource2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.hw13_cource2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw13Cource2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
